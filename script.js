63671cd2db3ec47b21e56bcf505bf03c1c78280a
swt4ajt <64845845+swt4ajt@users.noreply.github.com>
2025-09-03 11:52:16 +0100

    feat: include script.js in document_head.hbs and bump version to 4.11.49 in manifest.json

--- script.js
+++ script.js
@@ -1,13 +1,381 @@
-import { initNavigation } from "./src/navigation.js";
-import { initMultibrandFilter } from "./src/multibrandFilter.js";
-import { initDropdowns } from "./src/dropdown.js";
-import { initShare } from "./src/share.js";
-import { initSearch } from "./src/search.js";
-import { initFocus } from "./src/focus.js";
+(function () {
+  'use strict';
 
-initNavigation();
-initMultibrandFilter();
-initDropdowns();
-initShare();
-initSearch();
-initFocus();
+  // Key map
+  const ENTER = 13;
+  const ESCAPE = 27;
+
+  function toggleNavigation(toggle, menu) {
+    const isExpanded = menu.getAttribute("aria-expanded") === "true";
+    menu.setAttribute("aria-expanded", !isExpanded);
+    toggle.setAttribute("aria-expanded", !isExpanded);
+  }
+
+  function closeNavigation(toggle, menu) {
+    menu.setAttribute("aria-expanded", false);
+    toggle.setAttribute("aria-expanded", false);
+    toggle.focus();
+  }
+
+  // Navigation
+  window.addEventListener("DOMContentLoaded", () => {
+    const menuButton = document.querySelector(".header .menu-button-mobile");
+    const menuList = document.querySelector("#user-nav-mobile");
+    menuButton.addEventListener("click", (event) => {
+      event.stopPropagation();
+      toggleNavigation(menuButton, menuList);
+    });
+    menuList.addEventListener("keyup", (event) => {
+      if (event.keyCode === ESCAPE) {
+        event.stopPropagation();
+        closeNavigation(menuButton, menuList);
+      }
+    });
+    const collapsible = document.querySelectorAll(
+      ".collapsible-nav, .collapsible-sidebar"
+    );
+    collapsible.forEach((element) => {
+      const toggle = element.querySelector(
+        ".collapsible-nav-toggle, .collapsible-sidebar-toggle"
+      );
+      element.addEventListener("click", () => {
+        toggleNavigation(toggle, element);
+      });
+      element.addEventListener("keyup", (event) => {
+        if (event.keyCode === ESCAPE) {
+          closeNavigation(toggle, element);
+        }
+      });
+    });
+    // Multibrand Filter
+    const multibrandFilterLists = document.querySelectorAll(
+      ".multibrand-filter-list"
+    );
+    multibrandFilterLists.forEach((filter) => {
+      if (filter.children.length > 6) {
+        const trigger = filter.querySelector(".see-all-filters");
+        trigger.setAttribute("aria-hidden", false);
+        trigger.addEventListener("click", (event) => {
+          event.stopPropagation();
+          trigger.parentNode.removeChild(trigger);
+          filter.classList.remove("multibrand-filter-list--collapsed");
+        });
+      }
+    });
+    // Dropdowns
+    const dropdowns = [];
+    const dropdownToggles = document.querySelectorAll(".dropdown-toggle");
+    dropdownToggles.forEach((toggle) => {
+      const menu = toggle.nextElementSibling;
+      if (menu && menu.classList.contains("dropdown-menu")) {
+        dropdowns.push(new Dropdown(toggle, menu));
+      }
+    });
+    // Share
+    const links = document.querySelectorAll(".share a");
+    links.forEach((anchor) => {
+      anchor.addEventListener("click", (event) => {
+        event.preventDefault();
+        window.open(anchor.href, "", "height=500,width=500");
+      });
+    });
+    // Search
+    const searchForms = [...document.querySelectorAll(searchFormSelector)];
+    const searchInputs = searchForms.map((form) =>
+      form.querySelector("input[type='search']")
+    );
+    searchInputs.forEach((input) => {
+      appendClearSearchButton(input, input.closest(searchFormSelector));
+      input.addEventListener("keyup", clearSearchInputOnKeypress);
+      input.addEventListener("keyup", toggleClearSearchButtonAvailability);
+    });
+    // Focus
+    returnFocus();
+  });
+
+  // Dropdown class and helpers
+  const isPrintableChar = (str) => {
+    return str.length === 1 && str.match(/^\S$/);
+  };
+  function Dropdown(toggle, menu) {
+    this.toggle = toggle;
+    this.menu = menu;
+    this.menuPlacement = {
+      top: menu.classList.contains("dropdown-menu-top"),
+      end: menu.classList.contains("dropdown-menu-end"),
+    };
+    this.toggle.addEventListener("click", this.clickHandler.bind(this));
+    this.toggle.addEventListener("keydown", this.toggleKeyHandler.bind(this));
+    this.menu.addEventListener("keydown", this.menuKeyHandler.bind(this));
+    document.body.addEventListener("click", this.outsideClickHandler.bind(this));
+    const toggleId = this.toggle.getAttribute("id") || crypto.randomUUID();
+    const menuId = this.menu.getAttribute("id") || crypto.randomUUID();
+    this.toggle.setAttribute("id", toggleId);
+    this.menu.setAttribute("id", menuId);
+    this.toggle.setAttribute("aria-controls", menuId);
+    this.menu.setAttribute("aria-labelledby", toggleId);
+    this.menu.setAttribute("tabindex", -1);
+    this.menuItems.forEach((menuItem) => {
+      menuItem.tabIndex = -1;
+    });
+    this.focusedIndex = -1;
+  }
+  Dropdown.prototype = {
+    get isExpanded() {
+      return this.toggle.getAttribute("aria-expanded") === "true";
+    },
+    get menuItems() {
+      return Array.prototype.slice.call(
+        this.menu.querySelectorAll("[role='menuitem'], [role='menuitemradio']")
+      );
+    },
+    dismiss: function () {
+      if (!this.isExpanded) return;
+      this.toggle.removeAttribute("aria-expanded");
+      this.menu.classList.remove("dropdown-menu-end", "dropdown-menu-top");
+      this.focusedIndex = -1;
+    },
+    open: function () {
+      if (this.isExpanded) return;
+      this.toggle.setAttribute("aria-expanded", true);
+      this.handleOverflow();
+    },
+    handleOverflow: function () {
+      var rect = this.menu.getBoundingClientRect();
+      var overflow = {
+        right: rect.left < 0 || rect.left + rect.width > window.innerWidth,
+        bottom: rect.top < 0 || rect.top + rect.height > window.innerHeight,
+      };
+      if (overflow.right || this.menuPlacement.end) {
+        this.menu.classList.add("dropdown-menu-end");
+      }
+      if (overflow.bottom || this.menuPlacement.top) {
+        this.menu.classList.add("dropdown-menu-top");
+      }
+      if (this.menu.getBoundingClientRect().top < 0) {
+        this.menu.classList.remove("dropdown-menu-top");
+      }
+    },
+    focusByIndex: function (index) {
+      if (!this.menuItems.length) return;
+      this.menuItems.forEach((item, itemIndex) => {
+        if (itemIndex === index) {
+          item.tabIndex = 0;
+          item.focus();
+        } else {
+          item.tabIndex = -1;
+        }
+      });
+      this.focusedIndex = index;
+    },
+    focusFirstMenuItem: function () {
+      this.focusByIndex(0);
+    },
+    focusLastMenuItem: function () {
+      this.focusByIndex(this.menuItems.length - 1);
+    },
+    focusNextMenuItem: function (currentItem) {
+      if (!this.menuItems.length) return;
+      const currentIndex = this.menuItems.indexOf(currentItem);
+      const nextIndex = (currentIndex + 1) % this.menuItems.length;
+      this.focusByIndex(nextIndex);
+    },
+    focusPreviousMenuItem: function (currentItem) {
+      if (!this.menuItems.length) return;
+      const currentIndex = this.menuItems.indexOf(currentItem);
+      const previousIndex =
+        currentIndex <= 0 ? this.menuItems.length - 1 : currentIndex - 1;
+      this.focusByIndex(previousIndex);
+    },
+    focusByChar: function (currentItem, char) {
+      char = char.toLowerCase();
+      const itemChars = this.menuItems.map((menuItem) =>
+        menuItem.textContent.trim()[0].toLowerCase()
+      );
+      const startIndex =
+        (this.menuItems.indexOf(currentItem) + 1) % this.menuItems.length;
+      let index = itemChars.indexOf(char, startIndex);
+      if (index === -1) {
+        index = itemChars.indexOf(char, 0);
+      }
+      if (index > -1) {
+        this.focusByIndex(index);
+      }
+    },
+    outsideClickHandler: function (e) {
+      if (
+        this.isExpanded &&
+        !this.toggle.contains(e.target) &&
+        !e.composedPath().includes(this.menu)
+      ) {
+        this.dismiss();
+        this.toggle.focus();
+      }
+    },
+    clickHandler: function (event) {
+      event.stopPropagation();
+      event.preventDefault();
+      if (this.isExpanded) {
+        this.dismiss();
+        this.toggle.focus();
+      } else {
+        this.open();
+        this.focusFirstMenuItem();
+      }
+    },
+    toggleKeyHandler: function (e) {
+      const key = e.key;
+      switch (key) {
+        case "Enter":
+        case " ":
+        case "ArrowDown":
+        case "Down": {
+          e.stopPropagation();
+          e.preventDefault();
+          this.open();
+          this.focusFirstMenuItem();
+          break;
+        }
+        case "ArrowUp":
+        case "Up": {
+          e.stopPropagation();
+          e.preventDefault();
+          this.open();
+          this.focusLastMenuItem();
+          break;
+        }
+        case "Esc":
+        case "Escape": {
+          e.stopPropagation();
+          e.preventDefault();
+          this.dismiss();
+          this.toggle.focus();
+          break;
+        }
+      }
+    },
+    menuKeyHandler: function (e) {
+      const key = e.key;
+      const currentElement = this.menuItems[this.focusedIndex];
+      if (e.ctrlKey || e.altKey || e.metaKey) {
+        return;
+      }
+      switch (key) {
+        case "Esc":
+        case "Escape": {
+          this.dismiss();
+          this.toggle.focus();
+          break;
+        }
+        case "ArrowDown":
+        case "Down": {
+          this.focusNextMenuItem(currentElement);
+          break;
+        }
+        case "ArrowUp":
+        case "Up": {
+          this.focusPreviousMenuItem(currentElement);
+          break;
+        }
+        case "Home":
+        case "PageUp": {
+          this.focusFirstMenuItem();
+          break;
+        }
+        case "End":
+        case "PageDown": {
+          this.focusLastMenuItem();
+          break;
+        }
+        case "Tab": {
+          if (e.shiftKey) {
+            this.dismiss();
+            this.toggle.focus();
+          } else {
+            this.dismiss();
+          }
+          break;
+        }
+        default: {
+          if (isPrintableChar(key)) {
+            this.focusByChar(currentElement, key);
+          }
+        }
+      }
+    },
+  };
+
+  // Search helpers
+  function debounce(callback, wait) {
+    let timeoutId = null;
+    return (...args) => {
+      window.clearTimeout(timeoutId);
+      timeoutId = window.setTimeout(() => {
+        callback.apply(null, args);
+      }, wait);
+    };
+  }
+  const searchFormFilledClassName = "search-has-value";
+  const searchFormSelector = "form[role='search']";
+  function clearSearchInput(event) {
+    event.target.closest(searchFormSelector).classList.remove(searchFormFilledClassName);
+    let input;
+    if (event.target.tagName === "INPUT") {
+      input = event.target;
+    } else if (event.target.tagName === "BUTTON") {
+      input = event.target.previousElementSibling;
+    } else {
+      input = event.target.closest("button").previousElementSibling;
+    }
+    input.value = "";
+    input.focus();
+  }
+  function clearSearchInputOnKeypress(event) {
+    const searchInputDeleteKeys = ["Delete", "Escape"];
+    if (searchInputDeleteKeys.includes(event.key)) {
+      clearSearchInput(event);
+    }
+  }
+  function buildClearSearchButton(inputId) {
+    const button = document.createElement("button");
+    button.setAttribute("type", "button");
+    button.setAttribute("aria-controls", inputId);
+    button.classList.add("clear-button");
+    const buttonLabel = window.searchClearButtonLabelLocalized;
+    const icon = `<svg xmlns='http://www.w3.org/2000/svg' width='12' height='12' focusable='false' role='img' viewBox='0 0 12 12' aria-label='${buttonLabel}'><path stroke='currentColor' stroke-linecap='round' stroke-width='2' d='M3 9l6-6m0 6L3 3'/></svg>`;
+    button.innerHTML = icon;
+    button.addEventListener("click", clearSearchInput);
+    button.addEventListener("keyup", clearSearchInputOnKeypress);
+    return button;
+  }
+  function appendClearSearchButton(input, form) {
+    const searchClearButton = buildClearSearchButton(input.id);
+    form.append(searchClearButton);
+    if (input.value.length > 0) {
+      form.classList.add(searchFormFilledClassName);
+    }
+  }
+  const toggleClearSearchButtonAvailability = debounce((event) => {
+    const form = event.target.closest(searchFormSelector);
+    form.classList.toggle(
+      searchFormFilledClassName,
+      event.target.value.length > 0
+    );
+  }, 200);
+
+  // Focus helpers
+  const key = "returnFocusTo";
+  function saveFocus() {
+    const activeElementId = document.activeElement.getAttribute("id");
+    sessionStorage.setItem(key, "#" + activeElementId);
+  }
+  function returnFocus() {
+    const returnFocusTo = sessionStorage.getItem(key);
+    if (returnFocusTo) {
+      sessionStorage.removeItem("returnFocusTo");
+      const returnFocusToEl = document.querySelector(returnFocusTo);
+      returnFocusToEl && returnFocusToEl.focus && returnFocusToEl.focus();
+    }
+  }
+
+})();
